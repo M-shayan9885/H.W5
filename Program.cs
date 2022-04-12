f81600712378641d70304a4ba3ba70187f98d97f
shayan <72066443+shayanmashayekhy@users.noreply.github.com>
2022-04-12 14:40:56 +0430

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string file;
+string destinationfile;
+Console.WriteLine("please enter file location :");
+file = Console.ReadLine();
+Console.WriteLine("please enter destinationfile :");
+destinationfile  = Console.ReadLine();
+try
+{
+    File.Copy(file, destinationfile, true);
+}
+catch (IOException iox)
+{
+    Console.WriteLine(iox.Message);
+}
+foreach(file in Directory.GetFiles(destinationfile))
+{
+    if (File.Exists(file))
+    {
+        Console.WriteLine("file is exists");
+    }
+}
