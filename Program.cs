08b02319f24227500c671053514c6b52fa085c46
shayan <72066443+shayanmashayekhy@users.noreply.github.com>
2022-04-12 14:41:56 +0430

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,21 +1,37 @@
-﻿string file;
-string destinationfile;
-Console.WriteLine("please enter file location :");
-file = Console.ReadLine();
-Console.WriteLine("please enter destinationfile :");
-destinationfile  = Console.ReadLine();
-try
+﻿List<person> persons = new List<person> ();
+
+persons.Add(new person(1, "shayan", 23,"05/21/2014 12:32","ffdfdfrgregrg",123));
+persons.Add(new person(2, "hamid", 24,"343434","sfgdgdfgd",454));
+persons.Add(new person(3, "hamed", 19,"34343434","sdfdfgdgd",23434));
+
+
+Console.WriteLine("please select action :");
+Console.ReadLine();
+Console.WriteLine("1-sort name of person by 20+ age");
+Console.WriteLine("2-person who date of birth under 1999");
+Console.WriteLine("3-person who has equle birtday");
+Console.WriteLine("4-person sort by id && remove person number 4 of list");
+Console.WriteLine("5-person sort by id && remove person number 50 till number 80");
+Console.WriteLine("6-view person who has max age in list");
+Console.WriteLine("7-view report of there is double of id");
+Console.WriteLine("8-view person who live in tehran");
+Console.WriteLine("9-view person who live in tehran + has same name");
+Console.WriteLine("10-person has 1.2.3 in id");
+Console.WriteLine("11-person who has age above 25+");
+Console.WriteLine("ATTENTION");
+Console.WriteLine("in number 11 you can only see soctioalsecuritynumber with adress");
+
+var action = Convert.ToInt32(Console.ReadLine());
+
+foreach (var person in persons)
 {
-    File.Copy(file, destinationfile, true);
-}
-catch (IOException iox)
-{
-    Console.WriteLine(iox.Message);
-}
-foreach(file in Directory.GetFiles(destinationfile))
-{
-    if (File.Exists(file))
+    switch (action)
     {
-        Console.WriteLine("file is exists");
+        case 11:
+            {
+                
+                break;
+            }
     }
 }
+
---
+++ person.cs
@@ -0,0 +1,19 @@
+﻿public class person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string DateTime;
+    public string Adress { get; set; }
+    public int Sociolsecuritynumber { get; set; }
+    public person (int id, string name, int age,string dateTime,string adress,int socialsecuritynumber)
+    {
+        Id = id;
+        Name = name;
+        Age = age;
+        DateTime = dateTime;
+        Adress = adress;
+        Sociolsecuritynumber = socialsecuritynumber;
+    }
+        
+}
